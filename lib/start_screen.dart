97f0ff91c03a0cf0af9f015de069b07a2c485e1a
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-07 03:02:00 +0530

    images

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,35 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Center(child: Text('start screen'));
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+        ),
+        const SizedBox(
+          height: 80,
+        ),
+        const Text(
+          'Learn flutter the fun way!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.red,
+            ),
+            child: const Text('Start Quiz')),
+      ],
+    ));
   }
 }
