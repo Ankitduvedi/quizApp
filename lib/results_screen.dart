a6efddbe32835d344467e4dcc226dbe492935ca6
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-13 03:22:41 +0530

    result

---
+++ lib/results_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity, //use as much width as possible
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('you answerd 4 questions'),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text('list of answrs'),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Restart Quiz'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
